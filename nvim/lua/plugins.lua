0fe4f4af252b3a48d6111b770930d134814e6c62
Ishaan Arora <42387090+pulsar17@users.noreply.github.com>
2022-06-23 12:18:46 +0530

    feat: switch to pylsp and format the language servers' list

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -154,7 +154,11 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
 -- Enable the following language servers
-local servers = {'pyright'}
+local servers = {
+    'pylsp', -- Python
+    -- 'pyright', -- Python
+    'ccls' -- C
+}
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
     on_attach = on_attach,
